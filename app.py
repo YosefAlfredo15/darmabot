3930198a4676e1237b1c0b1419645d8f9e55ae57
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2024-01-10 20:51:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -749,7 +749,9 @@ if "conversation" not in st.session_state:
 
 # Form untuk input pengguna
 with st.form(key='my_form'):
-    user_message = st.text_input("Anda:", value="").lower()
+    
+
+user_message= st.text_input("<p style='text-align: center; color: white;'>Anda:</p>", unsafe_allow_html=True)
 
     # Tombol kirim
     submit_button = st.form_submit_button("Kirim")
