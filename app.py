20f34a97288e8297337ec7e660516d2d5d177b16
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2024-01-10 19:59:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,11 @@ st.markdown(
 # Path gambar
 image_path = "chatbot.webp"  # Ganti dengan path gambar Anda
 
-# Menambahkan gambar di atas tulisan dengan lebar 100%
-col1, col2, col3 = st.beta_columns([1, 3, 1])
-col2.image(image_path, width=100, use_container_width=True)
+# Mendapatkan lebar halaman
+page_width = st.beta_get_page_width()
+
+# Menambahkan gambar di tengah halaman dengan lebar 100%
+st.image(image_path, width=page_width)
 
 # Menambahkan judul di tengah dengan warna kuning
 st.markdown("<h1 style='text-align: center; color: yellow;'>SI Darma Chatbot</h1>", unsafe_allow_html=True)
