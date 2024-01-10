66bd482f66770a3a5f41ae1759733bc803c29c28
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2024-01-10 14:36:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,21 +17,21 @@ nltk.download('stopwords')
 
 
 
-# URL gambar background di repositori GitHub
-background_image_url = "https://raw.githubusercontent.com/YosefAlfredo15/darmabot/main/"  # Ganti dengan URL gambar Anda
+# # URL gambar background di repositori GitHub
+# background_image_url = "https://raw.githubusercontent.com/YosefAlfredo15/darmabot/main/"  # Ganti dengan URL gambar Anda
 
-# Menambahkan gambar sebagai background dengan lebar 100%
-st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url('{background_image_url}');
-        background-size: cover;  /* Untuk menutupi seluruh area background */
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# # Menambahkan gambar sebagai background dengan lebar 100%
+# st.markdown(
+#     f"""
+#     <style>
+#     .stApp {{
+#         background-image: url('{background_image_url}');
+#         background-size: cover;  /* Untuk menutupi seluruh area background */
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 # with open("D:/TUGAS AKHIR/desain.html", "r") as file:
 #     html_code = file.read()
