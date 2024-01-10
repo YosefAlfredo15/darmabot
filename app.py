aa83b78af68f4bf6b19695e8a1b289db423f5c81
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2024-01-10 20:09:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
+# Path gambar
+image_path = "chatbot.webp"  # Ganti dengan path gambar Anda
+st.image(image_path, width=50)
 
 # Menambahkan judul di tengah dengan warna kuning
 # Menambahkan gambar di halaman utama
